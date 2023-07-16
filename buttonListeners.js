2e51c0ff3975c78c8714509891b1fc61cc7467ca
Ashneet <ashneetnagi21@gmail.com>
2023-07-16 11:52:50 -0400

    side bar addition

---
+++ buttonListeners.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var openButton = document.getElementById("openButton");
+    sidebar.classList.toggle("open");
+    openButton.classList.toggle("open");
+  }
