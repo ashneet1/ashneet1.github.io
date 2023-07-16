7548f5a2eb072d6c26af676ab9d7cf184a9b3fd3
Ashneet <ashneetnagi21@gmail.com>
2023-07-16 12:07:49 -0400

    testing

--- buttonListeners.js
+++
@@ -1,6 +0,0 @@
-function toggleSidebar() {
-    var sidebar = document.getElementById("sidebar");
-    var openButton = document.getElementById("openButton");
-    sidebar.classList.toggle("open");
-    openButton.classList.toggle("open");
-  }
--- eventListener.js
+++ eventListener.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('openButton').addEventListener('click', handleGetPuzzel)
+    document.getElementById('openButton').addEventListener('click', )
 })
