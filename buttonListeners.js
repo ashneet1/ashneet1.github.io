0492783643bc99f90028ece0e510048cb4e9b0bf
Ashneet <ashneetnagi21@gmail.com>
2023-07-22 21:42:52 -0400

    separating functionality

--- buttonListeners.js
+++ buttonListeners.js
@@ -4,3 +4,9 @@ function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("openNav").style.display = 'none';
   }
+
+function w3_close() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+  }
--- eventListener.js
+++ eventListener.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('openNAV').addEventListener('click',w3_open())
+    ocument.getElementById('closeNAV').addEventListener('click',w3_close())
 })
