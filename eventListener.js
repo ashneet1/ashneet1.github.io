d43f37e900afc1043894b90d1ae942de9f77e424
Ashneet <ashneetnagi21@gmail.com>
2023-07-22 21:59:30 -0400

    test

--- eventListener.js
+++ eventListener.js
@@ -1,4 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('openNAV').addEventListener('click',w3_open())
     ocument.getElementById('closeNAV').addEventListener('click',w3_close())
 })
